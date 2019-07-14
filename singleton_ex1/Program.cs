f5f6f37e2b1661a15e3f706a92ced0b49b19a7db
michaelyefet <52877613+michaelyefet@users.noreply.github.com>
2019-07-14 11:52:52 +0300

    Add files via upload
    Ex1 - singleton

---
+++ singleton_ex1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace singleton_ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton s1 = Singleton.GetInstance();
+            Singleton s2 = Singleton.GetInstance();
+            Singleton s3 = Singleton.GetInstance();
+
+            s1.printTime();
+            s2.printTime();
+            s3.printTime();
+        }
+    }
+}
---
+++ singleton_ex1/Singleton.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace singleton_ex1
+{
+    class Singleton
+    {
+        private static Singleton INSTANCE;
+
+        private static object key = new object();
+        private Singleton()
+        {
+
+        }
+
+        public static Singleton GetInstance()
+        {
+            if (INSTANCE != null)
+            {
+                return INSTANCE;
+            }
+
+            lock (key)
+            {
+                if (INSTANCE == null)
+                {
+                    INSTANCE = new Singleton();
+                }
+            }
+
+            return INSTANCE;
+        }
+
+        public void printTime()
+        {
+            Console.WriteLine(DateTime.Now);
+        }
+    }
+}
