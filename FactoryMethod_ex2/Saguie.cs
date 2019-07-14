060ef63f8a6e16fb5f36c789e2aabb79a715f309
michaelyefet <52877613+michaelyefet@users.noreply.github.com>
2019-07-14 14:16:15 +0300

    Add files via upload
    factory EX2

---
+++ FactoryMethod_ex2/Saguie.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod_ex2
+{
+    class Saguie : Vehicle
+    {
+        protected string MaxSpeed;
+
+        public Saguie(string model, float cylinder, string color, int numberOfWheels, string MaxSpeed) : base(model, cylinder, color, numberOfWheels)
+        {
+            this.MaxSpeed = MaxSpeed;
+        }
+
+        public override string ToString()
+        {
+            return "Saguie : " + base.ToString() + $" { MaxSpeed}";
+        }
+    }
+}
---
+++ FactoryMethod_ex2/Track.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod_ex2
+{
+    class Track : Vehicle
+    {
+        protected string WeightToCarry;
+
+        public Track(string model, float cylinder, string color, int numberOfWheels, string WeightToCarry) : base(model, cylinder, color, numberOfWheels)
+        {
+            this.WeightToCarry = WeightToCarry;
+        }
+
+        public override string ToString()
+        {
+            return "Track : " + base.ToString() + $" { WeightToCarry}";
+        }
+    }
+}
