28cf2c1cec3be82f69ad9edb28d386dd9245181e
michaelyefet <52877613+michaelyefet@users.noreply.github.com>
2019-07-15 10:27:32 +0300

    Add files via upload
    design pattern chain of resp EX5

---
+++ ChainOfResp_EX5/BillHandlerBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainOfResp_EX5
+{
+    public abstract class BillHandlerBase
+    {
+
+        protected BillHandlerBase next;
+
+        public abstract void Handle(int amount);
+
+        public void SetNext(BillHandlerBase next)
+        {
+            this.next = next;
+        }
+
+    }
+}
