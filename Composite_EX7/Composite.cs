b5e15b03b05f210f36ae44f6dde831821e9125d9
michaelyefet <52877613+michaelyefet@users.noreply.github.com>
2019-07-15 11:31:54 +0300

    design patterns composite EX7

---
+++ Composite_EX7/Composite.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite_EX7
+{
+    public class Composite : Component
+    {
+
+        IList<Component> childs = new List<Component>();
+
+        public Composite(string name) : base(name)
+        {
+        }
+
+        public override void AddChild(Component c)
+        {
+            childs.Add(c);
+        }
+
+        public override IList<Component> GetChilds()
+        {
+            return childs;
+        }
+
+        public override void RemoveChild(Component c)
+        {
+            childs.Remove(c);
+        }
+
+        public override void Draw(string space)
+        {
+            Console.WriteLine(space + this.name);
+            foreach (Component c in childs)
+            {
+                c.Draw(space + "    ");
+            }
+        }
+        public override int Count()
+        {
+            int counter = 0;
+            foreach (Component c in childs)
+            {
+                counter += c.Count();
+            }
+            return counter;
+        }
+    }
+}
