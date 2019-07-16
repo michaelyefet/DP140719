aedbe17cfc0c6826317ebf07d4dd505b9fd513d4
michaelyefet <52877613+michaelyefet@users.noreply.github.com>
2019-07-16 15:48:24 +0300

    design patterns templateBase EX14

---
+++ TemplateBase/TemplateBase/MongoDeleteQuery.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateBase
+{
+    class MongoDeleteQuery : MongoTemplateBase
+    {
+        protected override void ExecuteQuery()
+        {
+            Console.WriteLine("Delete from DB im MongoDB .....");
+        }
+    }
+}
---
+++ TemplateBase/TemplateBase/MongoSelectQuery.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateBase
+{
+    class MongoSelectQuery : MongoTemplateBase
+    {
+        protected override void ExecuteQuery()
+        {
+            Console.WriteLine("Select * from DB im MongoDB .....");
+        }
+    }
+}
