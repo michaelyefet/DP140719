6120a2d652889303adbfabcb8336afcc31df1ef1
michaelyefet <52877613+michaelyefet@users.noreply.github.com>
2019-07-17 16:39:28 +0300

    design patterns visitor

---
+++ Visitor/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Visitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Liquor vodka = new Liquor(95);
+            Perfume calvin = new Perfume(300);
+            ShopRent shopRent = new ShopRent(4500);
+
+            IVisitor endOfYearVisitor = new EndOfYearTax();
+            IVisitor oneShekelSale = new OneShekelSale();
+
+            Console.WriteLine(vodka);
+            Console.WriteLine($"vodka after tax = {vodka.Accept(endOfYearVisitor)}");
+            Console.WriteLine($"vodka after sale = {vodka.Accept(oneShekelSale)}");
+            Console.WriteLine(calvin);
+            Console.WriteLine($"calvin after tax = {calvin.Accept(endOfYearVisitor)}");
+            Console.WriteLine($"calvin after sale = {calvin.Accept(oneShekelSale)}");
+            Console.WriteLine(shopRent);
+            Console.WriteLine($"shopRent after tax = {shopRent.Accept(endOfYearVisitor)}");
+            Console.WriteLine($"shopRent after sale = {shopRent.Accept(oneShekelSale)}");
+        }
+    }
+}
---
+++ Visitor/ShopRent.cs
@@ -0,0 +1,21 @@
+﻿namespace Visitor
+{
+    public class ShopRent : IVisitable
+    {
+        public double Price {get; set;}
+        public ShopRent(double price)
+        {
+            this.Price = price;
+        }
+
+        public double Accept(IVisitor visitor)
+        {
+            return visitor.Visit(this);
+        }
+
+        public override string ToString()
+        {
+            return $"ShopRent price : {Price}";
+        }
+    }
+}
