8891661717c8680104479f490dc06d002256a532
michaelyefet <52877613+michaelyefet@users.noreply.github.com>
2019-07-18 15:17:09 +0300

    final test answer to question 5

---
+++ FinalTest-180719/FinalTest_EX5/FinalTest_EX5/FatalHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalTest_EX5
+{
+    public class FatalHandler : LogHandlerBase
+    {
+        protected const string ERROR_FILE_PATH = @"C:\Users\HackerU\source\Exam\MichaelErrorLog\log.txt";
+        private const int LEVEL = 1;
+        public override void Handle(string log, int level)
+        {
+            if (level == LEVEL)
+            {
+                Console.WriteLine("FATAL: " + log);
+                WriteToFile(log);
+            }
+            else if (nextHandelr != null)
+            {
+                nextHandelr.Handle(log, level);
+            }
+        }
+
+        private void WriteToFile(string log)
+        {
+            Console.WriteLine("Writing fatal to file...");
+            File.AppendAllText(ERROR_FILE_PATH, Environment.NewLine + "FATAL: " + log);
+        }
+    }
+}
---
+++ FinalTest-180719/FinalTest_EX5/FinalTest_EX5/LogHandlerBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalTest_EX5
+{
+    public abstract class LogHandlerBase
+    {
+        protected LogHandlerBase nextHandelr;
+        public abstract void Handle(string log, int level);
+
+        public void SetNext(LogHandlerBase nextHandelr)
+        {
+            this.nextHandelr = nextHandelr;
+        }
+    }
+}
